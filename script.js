f83f60b344d79cf3ddd12b4e7292ee01125f5ab9
Michael Spica <michael@huginnandmuninntech.com>
2022-01-14 15:24:23 -0700

    Assignment

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-function compute()
+function updateRate() 
 {
-    p = document.getElementById("principal").value;
-    
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerHTML = rateval + " %";
 }
         
+function compute()
+{
+var principal = document.getElementById("principal").value;
+if(principal < 1 )
+    {
+       alert ("Enter a positive number");
+       document.getElementById("principal").focus();
+    }
+var rate = document.getElementById("rate").value;
+var years = document.getElementById("years").value;
+var interest = principal * years * rate /100;
+var year = new Date().getFullYear()+parseInt(years);
+var amount = parseFloat(interest);
+
+document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+}
+
+
+
